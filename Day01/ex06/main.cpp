62a63b7aaadc1b670959d8e30624a006046421d6
manacca <maximenaccache@gmail.com>
2021-03-10 10:27:35 +0100

    Day01 OK

--- Day01/ex06/main.cpp
+++ Day01/ex06/main.cpp
@@ -4,19 +4,19 @@
 
 int main()
 {
-	{
-	Weapon club = Weapon("crude spiked club");
-	HumanA bob("Bob", club);
+
+	Weapon clubA = Weapon("crude spiked club");
+	HumanA bob("Bob", clubA);
 	bob.attack();
-	club.setType("some other type of club");
+	clubA.setType("some other type of club");
 	bob.attack();
-	}
-	{
-	Weapon club = Weapon("crude spiked club");
+
+
+	Weapon clubB = Weapon("crude spiked club");
 	HumanB jim("Jim");
-	jim.setWeapon(club);
+	jim.setWeapon(clubB);
 	jim.attack();
-	club.setType("some other type of club");
+	clubB.setType("some other type of club");
 	jim.attack();
-	}
+
 }
